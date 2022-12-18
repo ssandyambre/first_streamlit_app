9c3c64d3fee7ae8c46a7bb1c35ee8167c799442d
Sandeep Ambre <120920022+ssandyambre@users.noreply.github.com>
2022-12-18 16:07:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale')
+streamlit.text('Egg')
