94a19ef4363b6f028bb70fc4336c5e01bfe33b9a
Sandeep Ambre <120920022+ssandyambre@users.noreply.github.com>
2022-12-20 16:30:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 streamlit.title('My Mom\'s New healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
